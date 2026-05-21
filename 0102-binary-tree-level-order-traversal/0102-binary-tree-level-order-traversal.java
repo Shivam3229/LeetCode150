852601b42e92d47b51afd4ff2c4038846115c918
Shivam Kumar Tripathi <shivamkrtripathi3229@gmail.com>
2026-05-22 04:10:05 +0530

    Time: 1 ms (95.62%), Space: 46.6 MB (79.07%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,32 +14,50 @@
  * }
  */
 class Solution {
+    public void order(List<List<Integer>> ans,int level,TreeNode root)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(level == ans.size())
+        {
+             ans.add(new ArrayList<>());
+            
+        }
+        ans.get(level).add(root.val);
+        order(ans,level+1,root.left);
+        order(ans,level+1,root.right);
+    }
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
-        if (root == null) {
-            return ans;
-        }
-        
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        
-        while (!q.isEmpty()) {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-            
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q.poll();
-                level.add(node.val);
-                
-                if (node.left != null) {
-                    q.add(node.left);
-                }
-                if (node.right != null) {
-                    q.add(node.right);
-                }
-            }
-            ans.add(level);
-        }
+        order(ans,0,root);
         return ans;
+
+        // if (root == null) {
+        //     return ans;
+        // }
+        
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+        
+        // while (!q.isEmpty()) {
+        //     int size = q.size();
+        //     List<Integer> level = new ArrayList<>();
+            
+        //     for (int i = 0; i < size; i++) {
+        //         TreeNode node = q.poll();
+        //         level.add(node.val);
+                
+        //         if (node.left != null) {
+        //             q.add(node.left);
+        //         }
+        //         if (node.right != null) {
+        //             q.add(node.right);
+        //         }
+        //     }
+        //     ans.add(level);
+        // }
+        // return ans;
     }
 }
