dcb348fcd58d5b4b7a7318d44b2685b66b839a91
Shivam Kumar Tripathi <shivamkrtripathi3229@gmail.com>
2026-06-04 08:30:39 +0530

    Time: 2 ms (100%), Space: 46.8 MB (16.4%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.java
@@ -1,16 +1,40 @@
+// class Solution {
+//     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+//         List<Integer> ans = new ArrayList<>();
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             for(int j=0;j<nums.length;j++){
+//                 if(Math.abs(i-j)<=k && nums[j]==key)
+//                 {
+//                     ans.add(i);
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public List<Integer> findKDistantIndices(int[] nums, int key, int k) {
+
         List<Integer> ans = new ArrayList<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums.length;j++){
-                if(Math.abs(i-j)<=k && nums[j]==key)
-                {
+        int start = 0;
+
+        for (int j = 0; j < nums.length; j++) {
+
+            if (nums[j] == key) {
+
+                int left = Math.max(start, j - k);
+                int right = Math.min(nums.length - 1, j + k);
+
+                for (int i = left; i <= right; i++) {
                     ans.add(i);
-                    break;
                 }
+
+                start = right + 1;
             }
         }
+
         return ans;
     }
 }
