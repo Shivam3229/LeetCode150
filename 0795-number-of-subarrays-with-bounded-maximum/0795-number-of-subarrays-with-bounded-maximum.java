146dc48dd24a2cd09552628893ea8194054cfa8d
Shivam Kumar Tripathi <shivamkrtripathi3229@gmail.com>
2026-06-14 22:05:21 +0530

    Time: 4 ms (34.13%), Space: 59.5 MB (52.48%) - LeetHub

--- 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.java
+++ 0795-number-of-subarrays-with-bounded-maximum/0795-number-of-subarrays-with-bounded-maximum.java
@@ -1,23 +1,45 @@
+// class Solution {
+
+//     private int count(int[] nums, int bound) {
+//         int ans = 0;
+//         int len = 0;
+
+//         for (int num : nums) {
+//             if (num <= bound) {
+//                 len++;
+//             } else {
+//                 len = 0;
+//             }
+
+//             ans += len;
+//         }
+
+//         return ans;
+//     }
+
+//     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+//         return count(nums, right) - count(nums, left - 1);
+//     }
+// }
 class Solution {
-
-    private int count(int[] nums, int bound) {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
         int ans = 0;
-        int len = 0;
+        int lastGreater = -1; // Last index where nums[i] > right
+        int lastValid = -1;   // Last index where nums[i] >= left
 
-        for (int num : nums) {
-            if (num <= bound) {
-                len++;
-            } else {
-                len = 0;
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] > right) {
+                lastGreater = i;
             }
 
-            ans += len;
+            if (nums[i] >= left) {
+                lastValid = i;
+            }
+
+            ans += Math.max(0, lastValid - lastGreater);
         }
 
         return ans;
-    }
-
-    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        return count(nums, right) - count(nums, left - 1);
     }
 }
