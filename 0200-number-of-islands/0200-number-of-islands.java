00b28f06d728341321c1141467eca9c455669de2
Shivam Kumar Tripathi <shivamkrtripathi3229@gmail.com>
2026-07-06 22:46:00 +0530

    Time: 10 ms (6.17%), Space: 51.4 MB (85.88%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+         int count = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+         boolean visited[][] = new boolean[n][m];
+         Queue<Node> q = new LinkedList<>();
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !visited[i][j])
+                {  count++;
+                    q.offer(new Node(i,j));
+                    visited[i][j] = true;
+                     int drow[] = {-1,0,1,0};
+                     int dcol[] = {0,-1,0,1};
+                  while(!q.isEmpty()){
+                   int r = q.peek().first;
+                   int c = q.peek().second;
+                      q.remove();
+                       for(int k=0;k<4;k++){
+                          int nrow = r + drow[k];
+                          int ncol = c + dcol[k];
+                         if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]=='1' && !visited[nrow][ncol]){
+                            visited[nrow][ncol] = true;
+                            q.offer(new Node(nrow,ncol));
+                         }
+                       }
+                  }
+                
+
+            
+                }
+            }
+         }
+         return count;
+        
+    }
+}
+
+class Node {
+    int first, second;
+
+    Node(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
